206c9c813de92baeee6f97f1b6cac23d2c275678
Lukoyanov84 <Lukoyanov_aa@mail.ru>
2018-04-23 14:53:10 +0300

    Добавлен ряд изменений затрагивающий 3 последние комита

--- index.php
+++ index.php
@@ -15,59 +15,60 @@
                 <script src="//api.bitrix24.com/api/v1/"></script>
                 <!-- Следующие 2 скрипта переписать на PHP-->
 		<script type="text/javascript" src="js/data.js"></script>
-		<script type="text/javascript" src="js/application.js"></script> 
+		<script type="text/javascript" src="js/application.js"></script>
+                <script type="text/javascript" src="js/yaMap.js"></script> 
                 <!-- Переделать на YA API 2.1-->
                 <script src="https://api-maps.yandex.ru/2.0/?load=package.full&amp;lang=ru-RU"" type="text/javascript"></script>
     </head>
     <body>
-        <h2>Basic Tabs</h2>
-        <p>Click tab strip to swap tab panel content.</p>
-        <div style="margin:20px 0 10px 0;"></div>
-        <div class="easyui-tabs" style="width:100%;height:500px">
-            <div title="Управление приложением" style="padding:10px">                
-                <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-add'" onclick="addBlocks();return false">Добавить блок</a>
-                <!-- Доделать реализацию-->
-                <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-remove'">Удалить блок</a>                               
-            </div>
-            <div title="Управление координатами" style="padding:10px">
-                <!-- Реализовать пагинацию-->
-                <table id="dg" title="Координаты" class="easyui-datagrid" style="width:100%;height:250px"
-			url= 'get.php'
-			toolbar="#toolbar" pagination="false"
-			rownumbers="true" fitColumns="true" singleSelect="true">
-                    <thead>
-                            <tr>
-                                    <th field="latitude" width="50">Долгота</th>
-                                    <th field="longitude" width="50">Широта</th>
-                                    
-                            </tr>
-                    </thead>
-                </table>
-                <div id="toolbar">
-                        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="create()">Добавить точку</a>
-                        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="edit()">Изменить точку</a>
-                        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroy()">Удалить точку</a>
+        <div id="app">
+            <h2>Basic Tabs</h2>
+            <p>Click tab strip to swap tab panel content.</p>
+            <div style="margin:20px 0 10px 0;"></div>
+            <div class="easyui-tabs" style="width:100%;height:500px">
+                <div title="Управление приложением" style="padding:10px">                
+                    <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-add'" onclick="addBlocks();return false">Добавить блок</a>
+                    <!-- Доделать реализацию-->
+                    <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-remove'">Удалить блок</a>                               
                 </div>
-            </div>
-            <div title="Как узнать координаты" style="padding:10px">
-                <div id="YMapsID"></div>
-                <div id="coord_form">                    
-                    <label for="latlongmet">Координаты метки:</label>
-                    <input id="latlongmet" class="Input-text">
-                
-                    <label>Масштаб: </label>
-                    <input id="mapzoom" class="Input-text">
-                    
-                    <label>Центр карты: </label>
-                    <input id="latlongcenter" class="Input-text">                
-                </div>
-            </div>            
-            <div title="О приложени" data-options="iconCls:'icon-help',closable:true" style="padding:10px">
-                This is the help content.
-            </div>
-        </div>	
+                <div title="Управление координатами" style="padding:10px">
+                    <!-- Реализовать пагинацию-->
+                    <table id="dg" title="Координаты" class="easyui-datagrid" style="width:100%;height:250px"
+                            url= 'get.php'
+                            toolbar="#toolbar" pagination="false"
+                            rownumbers="true" fitColumns="true" singleSelect="true">
+                        <thead>
+                                <tr>
+                                        <th field="latitude" width="50">Долгота</th>
+                                        <th field="longitude" width="50">Широта</th>
 
-	
+                                </tr>
+                        </thead>
+                    </table>
+                    <div id="toolbar">
+                            <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="create()">Добавить точку</a>
+                            <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="edit()">Изменить точку</a>
+                            <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroy()">Удалить точку</a>
+                    </div>
+                </div>
+                <div title="Как узнать координаты" style="padding:10px">
+                    <div id="YMapsID"></div>
+                    <div id="coord_form">                    
+                        <label for="latlongmet">Координаты метки:</label>
+                        <input id="latlongmet" class="Input-text">
+
+                        <label>Масштаб: </label>
+                        <input id="mapzoom" class="Input-text">
+
+                        <label>Центр карты: </label>
+                        <input id="latlongcenter" class="Input-text">                
+                    </div>
+                </div>            
+                <div title="О приложени" data-options="iconCls:'icon-help',closable:true" style="padding:10px">
+                    This is the help content.
+                </div>
+            </div>
+        </div>  
 	<div id="dlg" class="easyui-dialog" style="width:400px;height:280px;padding:10px 20px"
 			closed="true" buttons="#dlg-buttons">
 		<div class="ftitle">Координата</div>
@@ -87,8 +88,16 @@
 		<a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="save()" style="width:90px">Ок</a>
 		<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Отмена</a>
 	</div>
-	
-        
+	<script>	
+            $(document).ready(function () {
+                BX24.init(function(){
+                    app.saveFrameWidth();                                
+                    app.resizeFrame();                     
+                    
+                    ymaps.ready(init);
+                });
+            });
+        </script>        
     </body>
 </html>
 
